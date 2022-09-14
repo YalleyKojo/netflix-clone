81fe24c622acf4292110036c9b9f71d44532c0b2
YalleyKojo <yalleyabraham@yahoo.com>
2022-09-14 22:19:45 +0800

    3 com

--- src/App.js
+++ src/App.js
@@ -7,10 +7,29 @@ function App() {
     <div className="App">
       <Row 
       title="Netflix Originals"
-      fetchapi={requests.fetchNetflixOriginals}/>
+      fetchapi={requests.fetchNetflixOriginals}
+      isLargeRow/>
       <Row
       title="Trending"
       fetchapi={requests.fetchTrending}/>
+       <Row 
+      title="Top Rated"
+      fetchapi={requests.fetchTopRated}/>
+       <Row 
+      title="Action Movies"
+      fetchapi={requests.fetchActionMovies}/>
+       <Row 
+      title="Comedy Movies"
+      fetchapi={requests.fetchComedyMovies}/>
+       <Row 
+      title="Horror Movies"
+      fetchapi={requests.fetchHorrorMovies}/>
+       <Row 
+      title="Romance Movies"
+      fetchapi={requests.fetchRomanceMovies}/>
+       <Row 
+      title="Documentaries"
+      fetchapi={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import geturl from "./geturl"
 import "./Row.css"
 
-function Row({title,fetchapi}) {
+function Row({title,fetchapi,isLargeRow}) {
   const imageBase="https://image.tmdb.org/t/p/original/";
     const [movies,setMovies]=useState([]);
 
@@ -36,8 +36,8 @@ function Row({title,fetchapi}) {
      movies.map(movie=>(
       <img 
       key={movie.id}
-      className='row_poster'
-      src={`${imageBase}${movie.poster_path}`} alt={movie.name}/>
+      className={`row_poster ${isLargeRow && "row_posterLarge"} `}
+      src={`${imageBase}${isLargeRow?movie.poster_path:movie.backdrop_path}`} alt={movie.name}/>
      ))
      }
 
