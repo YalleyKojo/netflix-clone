0d1c77268cb29cc9646f5d766ce9c0c19efb2dd9
YalleyKojo <yalleyabraham@yahoo.com>
2022-09-23 21:18:02 +0800

    final com

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,15 +4,28 @@ import "./Nav.css"
 import {useEffect} from 'react'
 function Navbar() {
   const [show,handleShow]=useState(false);
+// handle scroll event 
+  const handleSroll=()=>{  
+    if(window.scrollY>100){
+      handleShow(true)
+    }
+    else handleShow(false);
+  }
 
+// add scroll transition effect when window is scroll down more than a 100px
+   useEffect(()=>{
+        window.addEventListener("scroll",handleSroll)
 
-   
+        return ()=>{
+  window.removeEventListener("scroll",handleSroll)
+        }
+   },[])
 
 
   return (
 
 
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
 
         <img className='nav_logo'
         src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Netflix123123.png'
--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import geturl from "./geturl"
 import "./Row.css"
+import Youtube from 'react-youtube'
+import movieTrailer from "movie-trailer"
 
 function Row({title,fetchapi,isLargeRow}) {
   const imageBase="https://image.tmdb.org/t/p/original/";
     const [movies,setMovies]=useState([]);
+    const [trailer,setTrailer]=useState('')
 
     useEffect(()=>{
 
@@ -25,26 +28,52 @@ function Row({title,fetchapi,isLargeRow}) {
        getmovies();
 
     },[fetchapi])
+
+    const opts={
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+    
+const handleClick=(movie)=>{
+  if (trailer){
+    setTrailer('')
+  }
+  else{
+    movieTrailer(movie?.name||'').then(url=>{
+    const urlparams= new URLSearchParams(new URL(url).search)
+      setTrailer(urlparams.get('v'))
+    }).catch(err=>console.log(err))
+  }
+}
+    
   return (
-    <div classname="row">
+            <><div classname="row">
       <h2>{title}</h2>
 
       <div className='row_posters'>
 
-     
-      { 
-     movies.map(movie=>(
-      <img 
-      key={movie.id}
-      className={`row_poster ${isLargeRow && "row_posterLarge"} `}
-      src={`${imageBase}${isLargeRow?movie.poster_path:movie.backdrop_path}`} alt={movie.name}/>
-     ))
-     }
+
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+            onClick={()=>handleClick(movie)}
+            className={`row_poster ${isLargeRow && "row_posterLarge"} `}
+            src={`${imageBase}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+        ))}
 
       </div>
 
     </div>
+    
+    <div>
+        {trailer && <Youtube videoId={trailer} opts={opts} />}
+      </div></>
+            
   )
-}
+    }
 
 export default Row
